7225db693c5aa551bd738e2b20de1b5d83e1369a
rahul <sandipkumar2024@gmail.com>
2024-10-13 01:33:05 +0530

    added cli-arg parsing for operation type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#![allow(dead_code)]
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    get_cli_args();
 }
+
+
+fn get_cli_args() {
+    // check the length of args
+    let args = env::args();
+
+    let args: Vec<String> = args.map(|x| x.parse().unwrap()).collect();
+    let args = args[1..].to_vec();
+
+    if args.is_empty() {
+        println!("No arguments specified, see --help for usage")
+    }
+    let oper = &args[0];
+    println!("{oper}");
+}
+
+#[derive(Debug)]
+enum OpType {
+    Find,
+    Create,
+    Edit,
+}
+
+
+
+
---
+++ todo.md
@@ -0,0 +1,2 @@
+1. Build the cli args parser
+
