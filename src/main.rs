0beffb3599b89bc042fa8dc3e0382aaab93e4c27
rahul <sandipkumar2024@gmail.com>
2024-10-13 23:08:45 +0530

    operation parsed

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,41 @@ fn main() {
 
 
 fn get_cli_args() {
-    // check the length of args
     let args = env::args();
-
     let args: Vec<String> = args.map(|x| x.parse().unwrap()).collect();
     let args = args[1..].to_vec();
 
     if args.is_empty() {
-        println!("No arguments specified, see --help for usage")
+        println!("no arguments specified, try --help for usage");
+        return;
     }
-    let oper = &args[0];
-    println!("{oper}");
+
+    match args[0].as_str() {
+        "find" => find(args),    
+        "create" => create(args),
+        "edit" => edit(args),
+        _ => { println!("invalid operation, try --help for usage"); }
+    }
 }
+
+fn find(args: Vec<String>) {
+    
+}
+
+fn create(args: Vec<String>) {
+
+}
+
+fn edit(args: Vec<String>) {
+
+}
+
+// for find second last value is always target-text and last value is the path
+// so it would always follow the structure below
+// find -----bunch of flag, value ---------- pattern path
+// so if we get just find and none of these then it is error 
+// if we get find and just one value, then find pattern in current directory(not recursviley)
+// if we get find pattern path, then nice
 
 #[derive(Debug)]
 enum OpType {
